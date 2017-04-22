79af14de67c5ded53102da839dfc9cdd8fcd5d7c
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-21 19:09:15 -0700

    display doctor's most recent practice and a landline phone number

--- js/doctor.js
+++ js/doctor.js
@@ -12,16 +12,24 @@ Doctor.prototype.getDoctors = function(symptoms, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptoms.join(",") + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&user_key=' + apiKey)
    .then(function(result) {
       var doctors = result.data;
-      // results = [];
-      // groups.forEach(function(group) {
-      //   results.push(group.profile);
-      // });
       displayDoctors(doctors);
       console.log(doctors);
     })
    .fail(function(error){
       $('#results').text(error.responseJSON.message);
     });
+};
+
+Doctor.prototype.getLandline = function(doctor) {
+  var landline = "";
+  doctor.practices[0].phones.forEach(function(phone) {
+    if (phone.type == "landline") {
+      landline = phone.number;
+    }
+  });
+  landline = landline.slice(0, 3) + "-" + landline.slice(3, 6) + "-" + landline.slice(6, 10);
+  console.log("landline: " + landline);
+  return landline;
 };
 
 exports.doctorModule = Doctor;
